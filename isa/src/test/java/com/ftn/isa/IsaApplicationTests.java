4fe204fc55c5d2ec25e82ad331f0daa4f9702771
EvilPopS <strahinjapopovic.evilpops@gmail.com>
2022-06-22 16:52:34 +0200

    - Testiran test za konflikt za rezervacije

--- isa/src/test/java/com/ftn/isa/IsaApplicationTests.java
+++ isa/src/test/java/com/ftn/isa/IsaApplicationTests.java
@@ -1,27 +1,24 @@
 package com.ftn.isa;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import com.ftn.isa.DTO.AnswerDTO;
-import com.ftn.isa.DTO.ClientProfileDTO;
+import com.ftn.isa.DTO.*;
 import com.ftn.isa.controllers.AdminController;
-import com.ftn.isa.model.Client;
-import com.ftn.isa.model.Request;
-import com.ftn.isa.model.User;
-import com.ftn.isa.services.AdminService;
-import com.ftn.isa.services.ClientService;
-import com.ftn.isa.services.RequestService;
+import com.ftn.isa.model.*;
+import com.ftn.isa.services.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.PessimisticLockingFailureException;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.ftn.isa.services.ReservationService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -30,11 +27,12 @@ public class IsaApplicationTests {
 	private ReservationService reservationService;
 	@Autowired
 	private ClientService clientService;
-
 	@Autowired
 	private AdminService adminService;
 	@Autowired
 	private RequestService requestService;
+	@Autowired
+	private CottageOwnerService cottageOwnerService;
 
 	@Test(expected = ObjectOptimisticLockingFailureException.class)
 	public void testAccDelRequests() throws Throwable {
@@ -151,6 +149,163 @@ public class IsaApplicationTests {
 			future2.get();
 		} catch (ExecutionException e) {
 			System.out.println("Exception from thread " + e.getCause().getClass());
+			throw e.getCause();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		executor.shutdown();
+	}
+
+	@Test(expected = ObjectOptimisticLockingFailureException.class)
+	@Rollback(true)
+	public void testClientAndOwnerRes() throws Throwable {
+
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		Future<?> future1 = executor.submit(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Startovan Thread 1");
+				Reservation newRes = new Reservation(LocalDateTime.now(), LocalDateTime.now().plusDays(3),
+						false, 150.0, true, false, "");
+				newRes.setCanceled(false);
+				newRes.setClient(clientService.findByEmail("client1@gmail.com"));
+				//Cottage cottage = cottageService.findById(4l); //na ovaj nacin se javi lazy iz nekog razloga
+				//newRes.setRental(cottage);
+
+				CottageOwner cottageOwner = cottageOwnerService.findByEmail("srdjan@gmail.com");
+				Cottage c = null;
+				for (Cottage cottage : cottageOwner.getCottages()){
+					c = cottage;
+					break;
+				}
+				newRes.setRental(c);
+				reservationService.saveReservation(newRes, c);    //poveze se rezervacija sa cottage drvence
+				try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
+			}
+		});
+		Future<?> future2 = executor.submit(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Startovan Thread 2");
+				try { Thread.sleep(150); } catch (InterruptedException ignored) { }
+				RegularResDTO resDTO = new RegularResDTO(4L, LocalDateTime.now(), LocalDateTime.now().plusDays(3),
+						"client1@gmail.com", 100.0);
+				reservationService.addNewRegularRes(resDTO, clientService.findByEmail("client1@gmail.com"), false, "COTTAGE_OWNER");
+			}
+		});
+		try {
+			future2.get();
+		} catch (ExecutionException e) {
+			System.out.println("Exception from thread " + e.getCause().getClass()); // u pitanju je bas OrptimisticLockingFailure
+			throw e.getCause();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		executor.shutdown();
+	}
+
+
+	@Test(expected = ObjectOptimisticLockingFailureException.class)
+	@Rollback(true)
+	public void testClientAndOwnerActionRes() throws Throwable {
+
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		Future<?> future1 = executor.submit(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Startovan Thread 1");
+				Reservation newRes = new Reservation(LocalDateTime.now(), LocalDateTime.now().plusDays(3),
+						false, 150.0, true, false, "");
+				newRes.setCanceled(false);
+				newRes.setClient(clientService.findByEmail("client1@gmail.com"));
+
+				CottageOwner cottageOwner = cottageOwnerService.findByEmail("srdjan@gmail.com");
+				Cottage c = null;
+				for (Cottage cottage : cottageOwner.getCottages()){
+					c = cottage;
+					break;
+				}
+				newRes.setRental(c);
+				reservationService.saveReservation(newRes, c);    //poveze se rezervacija sa cottage drvence
+				try { Thread.sleep(350); } catch (InterruptedException e) { e.printStackTrace(); }
+			}
+		});
+		Future<?> future2 = executor.submit(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Startovan Thread 2");
+				//try { Thread.sleep(150); } catch (InterruptedException e) { }// otprilike 150 milisekundi posle prvog threada krece da se izvrsava drugi
+
+				ActionResDTO resDTO = new ActionResDTO(LocalDateTime.now(), LocalDateTime.now().plusDays(3),
+						100.0, "", 4l);
+				reservationService.addNewActionRes(resDTO, "COTTAGE_OWNER");
+			}
+		});
+		try {
+			future2.get(); // podize ExecutionException za bilo koji izuzetak iz drugog child threada
+			//future1.get();
+		} catch (ExecutionException e) {
+			System.out.println("Exception from thread " + e.getCause().getClass()); // u pitanju je bas OrptimisticLockingFailure
+			throw e.getCause();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		executor.shutdown();
+	}
+
+	@Test(expected = ObjectOptimisticLockingFailureException.class)
+	@Rollback(true)
+	public void testChangingDataAndReservation() throws Throwable {
+
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		Future<?> future1 = executor.submit(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Startovan Thread 1");
+				Reservation newRes = new Reservation(LocalDateTime.now(), LocalDateTime.now().plusDays(3),
+						false, 150.0, true, false, "");
+				newRes.setCanceled(false);
+				newRes.setClient(clientService.findByEmail("client1@gmail.com"));
+				//Cottage cottage = cottageService.findById(4l); //na ovaj nacin se javi lazy iz nekog razloga
+				//newRes.setRental(cottage);
+
+				CottageOwner cottageOwner = cottageOwnerService.findByEmail("srdjan@gmail.com");
+				Cottage c = null;
+				for (Cottage cottage : cottageOwner.getCottages()){
+					c = cottage;
+					break;
+				}
+				newRes.setRental(c);
+				try { Thread.sleep(250); } catch (InterruptedException e) { e.printStackTrace(); }
+				reservationService.saveReservation(newRes, c);    //poveze se rezervacija sa cottage drvence
+			}
+		});
+		Future<?> future2 = executor.submit(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Startovan Thread 2");
+
+				CottageOwner cottageOwner = cottageOwnerService.findByEmail("srdjan@gmail.com");
+				Cottage c = null;
+				for (Cottage cottage : cottageOwner.getCottages()){
+					c = cottage;
+					break;
+				}
+
+				CottageDTO cottageDTO = new CottageDTO(c);
+				cottageOwnerService.save(cottageOwner, cottageDTO, new HashSet<>());
+			}
+		});
+		try {
+			future2.get();
+		} catch (ExecutionException e) {
+			System.out.println("Exception from thread " + e.getCause().getClass()); // u pitanju je bas OrptimisticLockingFailure
 			throw e.getCause();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
